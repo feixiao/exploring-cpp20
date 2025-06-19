c45064ac1a58be0eb4ed25ba8c102d8465fd3d3b
feixiao <feixiao2020@163.com>
2025-06-19 16:16:46 +0800

    update

--- code-preprocessor/chap04/list0406.cpp
+++ code-preprocessor/chap04/list0406.cpp
@@ -3,14 +3,11 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-   std::string shape;
-   int sides;
+int main() {
+  std::string shape;
+  int sides = 1;
 
-   std::cout << "Shape\t\tSides\n" <<
-                "-----\t\t-----\n";
-   std::cout << "Square\t\t" << 4 << '\n' <<
-                "Circle\t\t?\n";
-   std::cout << shape << '\t' << sides << '\n';
+  std::cout << "Shape\t\tSides\n" << "-----\t\t-----\n";
+  std::cout << "Square\t\t" << 4 << '\n' << "Circle\t\t?\n";
+  std::cout << shape << '\t' << sides << '\n';
 }
